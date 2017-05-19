a1415feac676ee85b941156aea8c45f25666bda0
ml <gorillazhip@126.com>
2017-05-19 23:45:01 +0800

    爬虫从爬取到存储成功

--- sql/acfun_fail_video.sql
+++ sql/acfun_fail_video.sql
@@ -1,5 +1,8 @@
 CREATE TABLE if not exists `acfun_fail_video` (
   `id` varchar(11) UNIQUE,
-  `FailReason` int(1) NULL ,
+  `FailReason` VARCHAR(1) NULL ,
+  `status_code` int(3) NULL ,
+  `type` int(1) NULL ,
+  `create_time` varchar(45) NULL ,
   PRIMARY KEY (`id`)
 )DEFAULT CHAR SET utf8;
